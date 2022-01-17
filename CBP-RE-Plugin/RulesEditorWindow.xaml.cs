dd054ad7a7e6a754fdc05b148385621b6670483f
MHLoppy <MHLoppy@users.noreply.github.com>
2022-01-17 22:32:15 +1100

    Add license headers

--- CBP-RE-Plugin/RulesEditorWindow.xaml.cs
+++ CBP-RE-Plugin/RulesEditorWindow.xaml.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/* 
+ * This file is part of CBP Rules Editor Plugin (CBP-RE-Plugin).
+ *
+ * CBP Rules Editor Plugin (CBP-RE-Plugin) is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * CBP Rules Editor Plugin (CBP-RE-Plugin) is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CBP Rules Editor Plugin (CBP-RE-Plugin). If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
